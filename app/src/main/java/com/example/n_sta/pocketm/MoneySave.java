e090691316a51c9408bffd50e4a666db19a77175
JungJaKyung <lostar77@naver.com>
2017-06-15 23:32:59 +0900

    Layout 변경

--- app/src/main/java/com/example/n_sta/pocketm/MoneySave.java
+++ app/src/main/java/com/example/n_sta/pocketm/MoneySave.java
@@ -1,7 +1,10 @@
 package com.example.n_sta.pocketm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by n-sta on 2017-06-13.
@@ -12,6 +15,16 @@ public class MoneySave extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.save_money);
+
+
+        Button btncallup = (Button) findViewById(R.id.btncallup);
+        btncallup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SettingLimit.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
--- app/src/main/java/com/example/n_sta/pocketm/SettingLimit.java
+++ app/src/main/java/com/example/n_sta/pocketm/SettingLimit.java
@@ -1,7 +1,10 @@
 package com.example.n_sta.pocketm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by n-sta on 2017-06-13.
@@ -12,5 +15,14 @@ public class SettingLimit extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.limit_setting);
+
+        Button btnPercent = (Button) findViewById(R.id.btnPercent);
+        btnPercent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DividePercent.class);
+                startActivity(intent);
+            }
+        });
     }
 }
